5eafacc3e1898dc14b4745f39be6eeee93af517a
NeoNova111 <carlo.fruehauf@t-online.de>
2020-01-31 22:54:48 +0100

    test commit

---
+++ Assets/Scripts/DisableRandom.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableRandom : MonoBehaviour
+{
+    public float breakTimer = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(breakOneOfInSeconds(GameObject.FindGameObjectsWithTag("Maschine"), breakTimer));
+    }
+
+    IEnumerator breakOneOfInSeconds(GameObject[] breakableMaschines, float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        while (true)
+        {
+            while (true)
+            {
+                bool breakableFound = false;
+                //checks if there is a breakable object left
+                foreach (GameObject breakable in breakableMaschines)
+                {
+                    if (breakable.GetComponent<ConveyorControll>().working)
+                    {
+                        breakableFound = true;
+                        break;
+                    }
+                }
+
+                if (!breakableFound) break;
+
+                //breaks random breakable object
+                int i = Random.Range(0, breakableMaschines.Length);
+                if (breakableMaschines[i].GetComponent<ConveyorControll>().working)
+                {
+                    breakableMaschines[i].GetComponent<ConveyorControll>().working = false;
+                    breakableMaschines[i].GetComponent<ConveyorControll>().switchArrows();
+                    break;
+                }
+            }
+            yield return new WaitForSeconds(seconds);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody playerRb;
+    public float movementSpeed;
+
+    private Vector3 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    }
+
+    private void moveCharacter(Vector3 movement)
+    {
+        playerRb.AddRelativeForce(movement * movementSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+}
